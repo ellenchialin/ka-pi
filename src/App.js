2c2596563f020b36aa9b24746acee382481559c4
ellenchen <chialin76@gmail.com>
2022-04-09 21:38:58 +0800

    Delete unused elements

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import {
   Drawer,
   DrawerOverlay,
   DrawerContent,
-  Flex,
-  IconButton,
 } from '@chakra-ui/react'
 // import { ColorModeSwitcher } from './ColorModeSwitcher'
-import { CgMenuLeft } from 'react-icons/cg'
 import Header from './components/Header'
 
 import SidebarContent from './components/SidebarContent'
@@ -31,28 +28,6 @@ function App() {
         </Drawer>
         <Box ml={{ base: 0, md: 60 }} transition=".3s ease">
           <Header onOpen={onOpen} />
-
-          {/*<Flex
-            as="header"
-            align="center"
-            justify="space-between"
-            w="full"
-            px="4"
-            bg="gray.800"
-            borderBottomWidth="1px"
-            borderColor="gray.700"
-            h="14"
-            display={{ base: 'block', md: 'none' }}
-          >
-            <IconButton
-              aria-label="主選單"
-              display={{ base: 'inline-flex', md: 'none' }}
-              onClick={onOpen}
-              icon={<CgMenuLeft />}
-              size="md"
-            />
-            {<ColorModeSwitcher />
-          </Flex>*/}
           <Box as="main" p="4">
             <Home />
           </Box>
