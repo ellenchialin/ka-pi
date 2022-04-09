88345d3adc09f4a7a0970bd3f827843b3ed5d798
ellenchen <chialin76@gmail.com>
2022-04-09 23:32:00 +0800

    Get user current location coords

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Home() {
+  const [userLatitude, setUserLatitude] = useState(null)
+  const [userLongitude, setUserLongitude] = useState(null)
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      alert('目前使用的瀏覽器版本不支援取得當前位置 😰 ')
+    }
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        setUserLatitude(position.coords.latitude)
+        setUserLongitude(position.coords.longitude)
+        console.log(position)
+      },
+      () => {
+        alert('請開啟允許取得當前位置，以獲得附近咖啡廳地圖 ☕️ ')
+      }
+    )
+  }, [])
+
   return <div>HOME</div>
 }
 
