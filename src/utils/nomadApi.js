7c5d89518ba90e17361cf9575e39be97fef5fc44
ellenchen <chialin76@gmail.com>
2022-04-12 02:01:39 +0800

    Add for work page

--- src/utils/nomadApi.js
+++ src/utils/nomadApi.js
@@ -1,4 +1,7 @@
 const nomad = {
+  getAllCafes() {
+    return fetch('/api/v1.2/cafes').then(response => response.json())
+  },
   getCafesByCity(city) {
     return fetch(`/api/v1.2/cafes/${city}`).then(response => response.json())
   },
