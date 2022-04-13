5aa244d6a65348f83ea7c18faf6de633fb29429c
ellenchen <chialin76@gmail.com>
2022-04-13 11:38:53 +0800

    Fix taipei and new taipei cafes bug

--- src/components/CityInfoCard.js
+++ src/components/CityInfoCard.js
@@ -2,14 +2,33 @@ import { Link } from 'react-router-dom'
 import { Flex, Heading, Text } from '@chakra-ui/react'
 import { HiOutlineArrowCircleRight } from 'react-icons/hi'
 
-function CityInfoCard({ hoveredCity, cityLinkEndpoint, isLoading, cityCafes }) {
+function CityInfoCard({
+  hoveredCity,
+  cityLinkEndpoint,
+  isLoading,
+  cityCafes,
+  taipeiCafes,
+  newTaipeiCafes,
+}) {
+  const getCafeNumbers = () => {
+    if (taipeiCafes.length > 0) {
+      console.log('Taipei Cafes: ', taipeiCafes)
+      return `共收錄 ${taipeiCafes.length} 間咖啡廳`
+    } else if (newTaipeiCafes.length > 0) {
+      console.log('New Taipei Cafes: ', newTaipeiCafes)
+      return `共收錄 ${newTaipeiCafes.length} 間咖啡廳`
+    } else {
+      return `共收錄 ${cityCafes.length} 間咖啡廳`
+    }
+  }
+
   return (
     <>
       <Heading as="h4" size="md" color="white">
         {hoveredCity}
       </Heading>
       <Text color="white" fontSize="0.875rem">
-        {isLoading ? '整理咖啡廳中...' : `共收錄 ${cityCafes.length} 間咖啡廳`}
+        {isLoading ? '整理咖啡廳中...' : getCafeNumbers()}
       </Text>
       <Flex align="center" mt="2">
         <HiOutlineArrowCircleRight color="#ecc94b" />
